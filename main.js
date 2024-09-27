71a729956693124e59eff2e6011d3c903b54534e
Jesus Bernat <contact@asedem.com>
2024-09-27 10:43:27 +0200

    arreglo en js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ document.getElementById('miFormulario').onsubmit = async function (e) {
 
     nombre = document.getElementById('nombre').value;
 
-    response = await fetch(`/cgi-bin/funciones.cgi?operacion=suma&n=${nombre}`);
+    response = await fetch(`/cgi-bin/funciones.cgi?n=${nombre}`);
     data = await response.text();
     document.getElementById('resultado').innerHTML = `Bienvenido, ${data}`;
 };
