dcc9c2430b89dc2210523dc4dcf26275a94674f8
Jesus Bernat <contact@asedem.com>
2024-09-27 13:21:08 +0200

    ahora sí

--- main.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('miFormulario').onsubmit = async function (e) {
-    e.preventDefault();
-
-    nombre = document.getElementById('nombre').value;
-
-    response = await fetch(`/cgi-bin/funciones.cgi?n=${nombre}`);
-    data = await response.text();
-    document.getElementById('resultado').innerHTML = `Bienvenido, ${data}`;
-};
